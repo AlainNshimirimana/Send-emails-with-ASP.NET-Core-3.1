4c7eb8f258799b7c06230c7869d0d511d79f8a0f
Alain Nshimirimana <alain.nshimirimana@gmail.com>
2021-01-18 21:29:04 -0800

    Created a MailRequest model to create parameters so the data can be passed on the service layer. Then configured mail settings in Appsettings.json

---
+++ Models/MailRequest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Send_Emails.Models
+{
+    public class MailRequest
+    {
+        // parameters such as email, subject, body, and list of attachements in the request model
+        // so that these data can be passed on to the service layer
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public List<IFormFile> Attachments { get; set; }
+    }
+}
